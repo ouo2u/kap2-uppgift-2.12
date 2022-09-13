ad53fb313ec12637ed88924a2eab2d15c6e5b762
ouo2u <abod98451@gmail.com>
2022-09-13 21:23:31 +0200

    Add project files.

---
+++ kap2 uppgift 2.12/Program.cs
@@ -0,0 +1,31 @@
+﻿   using System;
+
+   namespace Uppgift_2._12
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+            Console.WriteLine(" Hur många dagar vill du hyra billen? ");
+             string strdagarText = Console.ReadLine();
+            Console.WriteLine("hur många kilometer kommer du att köra?");
+            string strkilometer = Console.ReadLine();
+
+            int intdagarText = int.Parse(strdagarText);
+            int intkilometer = int.Parse(strkilometer);
+
+            int sum = 300 + intdagarText * 500 + intkilometer * 1 -500;
+
+            Console.WriteLine("Priset kommer at vara " + sum + "kr");
+
+
+
+         
+
+
+
+        }
+        }
+    }
+ 
